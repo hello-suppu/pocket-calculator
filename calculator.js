deb54106d536bbdf52a2ce3fcacbb200b79488c1
hello-suppu <124813893+hello-suppu@users.noreply.github.com>
2023-04-04 15:40:13 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,19 @@
+let display = document.getElementById("screen");
+display.style.backgroundColor = "white";
+display.classList.add("display");
+
+function allclear() {
+    display.value = "";
+}
+
+function clear() {
+    display.value = display.value.slice(0, -1)
+}
+
+function show(n) {
+    display.value += n;
+}
+
+function calc() {
+    display.value = eval(display.value);
+}
